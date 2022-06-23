726171b5c31af6781dd84504f498af652c474291
Александр Сергеевич Соловейко <solovejko00@mail.ru>
2022-06-23 18:01:47 +0300

    Use MySQL

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const fetch = require('node-fetch');
+const mysql = require("mysql2");
 
 let myKey = '0b5edc7455a336d544760ce639198bc9';
 
@@ -9,21 +9,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const dataBaseUrl = 'mongodb+srv://Solovejko:235689124578@cluster0.4fi9y.mongodb.net/DataBaseCities?retryWrites=true&w=majority'
-
-mongoose.connect(dataBaseUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-    if (err)
-        console.error(err);
-    else
-        console.log('Successfully connected');
+const connection = mysql.createConnection({
+    host: "sql4.freesqldatabase.com",
+    user: "sql4501675",
+    database: "sql4501675",
+    password: "im31Vurfz9"
 });
 
-const citySchema = new mongoose.Schema ({
-    _id: String,
-    name: String
+connection.connect(function(err){
+    if (err) {
+        return console.error("Ошибка: " + err.message);
+    }
+    else{
+        console.log("Подключение к серверу MySQL успешно установлено");
+    }
 });
-
-const city = mongoose.model('city', citySchema);
 
 app.options('*', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
@@ -38,12 +38,14 @@ app.route('/favorites')
     .get((req, res) => {
         res.set('Access-Control-Allow-Origin', '*');
 
-        city.find()
-            .exec((err, cities) => {
+        connection.query('SELECT * FROM city', (err, cities) => {
                 if (err)
                     res.send(err);
 
                 if (cities){
+                    for (var i = 0; i < cities.length; i++)
+                        cities[i].name = translit(cities[i].name, true);
+
                     res.send(cities);
                     console.log('Get /favorites');
                 }
@@ -53,17 +55,13 @@ app.route('/favorites')
         res.set("Access-Control-Allow-Origin", '*');
 
         const cityId = Date.now().toString();
+        req.body.name = translit(req.body.name);
 
-        let favorite = new city({
-            _id: cityId,
-            name: req.body.name
-        });
-
-        favorite.save(err => {
+        connection.query('INSERT INTO city (_id, name) VALUES (?, ?)', [cityId, req.body.name], err => {
             if (err)
                 res.send(err);
             else {
-                res.send({_id: Date.now()});
+                res.send({_id: cityId});
                 console.log('Post /favorites');
             }
         });
@@ -71,7 +69,7 @@ app.route('/favorites')
     .delete((req, res) => {
         res.set('Access-Control-Allow-Origin', '*');
 
-        city.deleteOne({_id: req.body.id}, (err) => {
+        connection.query('DELETE FROM `city` WHERE _id = ?', req.body.id, err => {
             if (err)
                 res.send(err);
             else {
@@ -113,3 +111,63 @@ app.get('/weather/coordinates', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started');
 })
+
+function translit(word, en){
+    var answer = '';
+    var converter = {
+        'а': '10',    'б': '17',    'в': '24',    'г': '31',    'д': '37',
+        'е': '11',    'ё': '18',    'ж': '25',    'з': '32',    'и': '38',
+        'й': '12',    'к': '19',    'л': '26',    'м': '33',    'н': '39',
+        'о': '13',    'п': '20',    'р': '27',    'с': '34',    'т': '40',
+        'у': '14',    'ф': '21',    'х': '28',    'ц': '35',    'ч': '41',
+        'ш': '15',    'щ': '22',    'ь': '29',    'ы': '36',    'ъ': '42',
+        'э': '16',    'ю': '23',    'я': '30',
+
+        'А': '43',    'Б': '50',    'В': '57',    'Г': '64',    'Д': '70',
+        'Е': '44',    'Ё': '51',    'Ж': '58',    'З': '65',    'И': '71',
+        'Й': '45',    'К': '52',    'Л': '59',    'М': '66',    'Н': '72',
+        'О': '46',    'П': '53',    'Р': '60',    'С': '67',    'Т': '73',
+        'У': '47',    'Ф': '54',    'Х': '61',    'Ц': '68',    'Ч': '74',
+        'Ш': '48',    'Щ': '55',    'Ь': '62',    'Ы': '69',    'Ъ': '75',
+        'Э': '49',    'Ю': '56',    'Я': '63',
+
+        '10': 'а',    '17': 'б',    '24': 'в',    '31': 'г',    '37': 'д',
+        '11': 'е',    '18': 'ё',    '25': 'ж',    '32': 'з',    '38': 'и',
+        '12': 'й',    '19': 'к',    '26': 'л',    '33': 'м',    '39': 'н',
+        '13': 'о',    '20': 'п',    '27': 'р',    '34': 'с',    '40': 'т',
+        '14': 'у',    '21': 'ф',    '28': 'х',    '35': 'ц',    '41': 'ч',
+        '15': 'ш',    '22': 'щ',    '29': 'ь',    '36': 'ы',    '42': 'ъ',
+        '16': 'э',    '23': 'ю',    '30': 'я',
+
+        '43': 'А',    '50': 'Б',    '57': 'В',    '64': 'Г',    '70': 'Д',
+        '44': 'Е',    '51': 'Ё',    '58': 'Ж',    '65': 'З',    '71': 'И',
+        '45': 'Й',    '52': 'К',    '59': 'Л',    '66': 'М',    '72': 'Н',
+        '46': 'О',    '53': 'П',    '60': 'Р',    '67': 'С',    '73': 'Т',
+        '47': 'У',    '54': 'Ф',    '61': 'Х',    '68': 'Ц',    '74': 'Ч',
+        '48': 'Ш',    '55': 'Щ',    '62': 'Ь',    '69': 'Ы',    '75': 'Ъ',
+        '49': 'Э',    '56': 'Ю',    '63': 'Я'
+    };
+
+    if (en){
+        var i = 0;
+        while (i < word.length){
+            if (converter[word[i] + word[i + 1]] === undefined){
+                answer += word[i];
+                i++;
+            } else {
+                answer += converter[word[i] + word[i + 1]];
+                i+=2;
+            }
+        }
+    } else {
+        for (var i = 0; i < word.length; ++i) {
+            if (converter[word[i]] === undefined) {
+                answer += word[i];
+            } else {
+                answer += converter[word[i]];
+            }
+        }
+    }
+
+    return answer;
+}
